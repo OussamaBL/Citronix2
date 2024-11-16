d0beaa2c803cb78bcea768c9146358ba0ec19fd3
OussamaBL <oussamaazrour@gmail.com>
2024-11-16 16:39:49 +0100

    Integration de global exception

---
+++ src/main/java/com/citronix/citronix/web/errors/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.citronix.citronix.web.errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationException(MethodArgumentNotValidException exceptions){
+        Map<String,String> errors=new HashMap<>();
+        exceptions.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(),error.getDefaultMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+}
